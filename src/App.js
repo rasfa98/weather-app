a5e16dbb659f63e2501d01b4e46713e7e0e2510f
Rasmus Falk <rf222fu@student.lnu.se>
2018-08-02 21:21:23 +0200

    Adding navbar component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Dashboard from './components/layout/Dashboard';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <Dashboard />
       </div>
     );
