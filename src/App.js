99c2a9829d70ed7309329448b7b140f09e068669
Rasmus Falk <rf222fu@student.lnu.se>
2018-08-02 21:17:52 +0200

    Adding dashboard component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+
 import './App.css';
+
+import Dashboard from './components/layout/Dashboard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <Dashboard />
       </div>
     );
   }
