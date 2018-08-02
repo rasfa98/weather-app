dad79cae034e43734ffce4db3f13f5ae8373ffd2
Rasmus Falk <rf222fu@student.lnu.se>
2018-08-02 22:05:24 +0200

    Adding search field to navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,9 +3,31 @@ import React, { Component } from 'react';
 class Navbar extends Component {
   render() {
     return (
-      <div>
-        <h1>Navbar</h1>
-      </div>
+      <nav className="navbar navbar-dark bg-primary">
+        <a href="#!" className="navbar-brand">
+          WeatherApp
+        </a>
+
+        <form className="form-inline">
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search..."
+            />
+
+            <ul className="navbar-nav ml-auto" />
+
+            <div className="input-group-append">
+              <button type="submit" className="btn btn-dark">
+                <span>
+                  <i className="fas fa-search" />
+                </span>
+              </button>
+            </div>
+          </div>
+        </form>
+      </nav>
     );
   }
 }
