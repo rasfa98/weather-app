d92f4ab66ec95213213f788718294c24f74ca13a
Rasmus Falk <rf222fu@student.lnu.se>
2018-08-03 11:56:27 +0200

    Adding default lon and lat coordinates

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,9 +1,16 @@
 import { GET_USER_POSITION } from '../actions/types';
 
-const initialState = {};
+const initialState = {
+  position: { lon: 18.06324, lat: 59.334591 }
+};
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case GET_USER_POSITION:
+      return {
+        ...state,
+        position: action.payload
+      };
     default:
       return state;
   }
