7d92dc5bfda5b6e2d6dd46479013d664919f6f40
Rasmus Falk <rf222fu@student.lnu.se>
2018-08-03 10:17:21 +0200

    Setting up redux store and weather reducer

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import './App.css';
 import Dashboard from './components/layout/Dashboard';
 import Navbar from './components/layout/Navbar';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Dashboard />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Navbar />
+          <Dashboard />
+        </div>
+      </Provider>
     );
   }
 }
