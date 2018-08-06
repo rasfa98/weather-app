97716a8ac4ecf3f6d45b0743462cd7ad92ec06ed
Rasmus Falk <rf222fu@student.lnu.se>
2018-08-06 20:20:50 +0200

    Removing button from search bar

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -4,15 +4,11 @@ class SearchBar extends Component {
   render() {
     return (
       <div className="input-group mb-4">
-        <input type="text" className="form-control" placeholder="Search..." />
-
-        <div className="input-group-append">
-          <button type="submit" className="btn btn-dark">
-            <span>
-              <i className="fas fa-search" />
-            </span>
-          </button>
-        </div>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter city..."
+        />
       </div>
     );
   }
