03a64dee1080b61aeb8d4bfc452fb223c4f68425
Rasmus Falk <rf222fu@student.lnu.se>
2018-08-12 18:52:08 +0200

    Adding settings reducer and types

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import weatherReducer from './weatherReducer';
 import userReducer from './userReducer';
 import errorReducer from './errorReducer';
+import settingsReducer from './settingsReducer';
 
 export default combineReducers({
   weather: weatherReducer,
   user: userReducer,
-  errors: errorReducer
+  errors: errorReducer,
+  settings: settingsReducer
 });
---
+++ src/reducers/settingsReducer.js
@@ -0,0 +1,23 @@
+import { CHANGE_API_KEY, CHANGE_TEMPERATURE_UNIT } from '../actions/types';
+
+const initialState = {
+  temperatureUnit: 'celsius'
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case CHANGE_TEMPERATURE_UNIT:
+      return {
+        ...state,
+        temperatureUnit: action.payload
+      };
+    case CHANGE_API_KEY:
+      return {
+        ...state,
+        apiKey: action.payload
+      };
+
+    default:
+      return state;
+  }
+}
