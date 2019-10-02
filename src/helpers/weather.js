8ad53190bc4aee1e2f4fea3625188c56b21511ee
Robert Ahlberg <rob.ahlberg@gmail.com>
2019-10-02 18:14:14 +0200

    Refactor if chain

--- src/helpers/weather.js
+++ src/helpers/weather.js
@@ -1,23 +1,22 @@
-export const weatherIcon = id => {
-  if (id === '01d') {
-    return 'clear-sky-day';
-  } else if (id === '01n') {
-    return 'clear-sky-night';
-  } else if (id === '02d') {
-    return 'few-clouds-day';
-  } else if (id === '02n') {
-    return 'few-clouds-night';
-  } else if (id === '03d' || id === '03n') {
-    return 'scattered-clouds';
-  } else if (id === '04d' || id === '04n') {
-    return 'broken-clouds';
-  } else if (id === '09d' || id === '09n' || id === '10d' || id === '10n') {
-    return 'rain';
-  } else if (id === '11d' || id === '11n') {
-    return 'thunderstorm';
-  } else if (id === '13d' || id === '13n') {
-    return 'snow';
-  } else if (id === '50d' || id === '50n') {
-    return 'fog';
-  }
+const iconTranslationMap = {
+  '01d': 'clear-sky-day',
+  '01n': 'clear-sky-night',
+  '02d': 'few-clouds-day',
+  '02n': 'few-clouds-night',
+  '03d': 'scattered-clouds',
+  '03n': 'scattered-clouds',
+  '04d': 'broken-clouds',
+  '04n': 'broken-clouds',
+  '09d': 'rain',
+  '09n': 'rain',
+  '10d': 'rain',
+  '10n': 'rain',
+  '11d': 'thunderstorm',
+  '11n': 'thunderstorm',
+  '13d': 'snow',
+  '13n': 'snow',
+  '50d': 'fog',
+  '50n': 'fog',
 };
+
+export const weatherIcon = id => iconTranslationMap[id];
